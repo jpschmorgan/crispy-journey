cad9bbd6226c56f44aa97c13ec8f12d3c5811535
jpschmorgan <jpschmorgan@users.noreply.github.com>
2026-01-06 12:25:02 +0200

    fix: add error messages

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+# Updated iteration 16
+def function_16():
+    """Helper function for feature 16"""
+    return True
+
+def process_data_16(data):
+    """Process data for iteration 16"""
+    if data:
+        return data.upper()
+    return None
